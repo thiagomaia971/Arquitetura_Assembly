5d7c301cee72aa83c5ac6a38902c38cdd2e1ab8d
Thiago Maia da Costa <thiago.maia971@gmail.com>
2016-05-19 11:04:31 -0300

    Correção

--- mycode.asm
+++ mycode.asm
@@ -3,24 +3,30 @@ org 100h
 mov AX, 0000h;   
 mov DS, AX;
       ;Configurando a int 00h
-mov [2], 0700h;  CS do local de memoria onde o tratamento da memoria esta
-mov [0], 0122h;  IP
+mov [2h], 0700h;  CS do local de memoria onde o tratamento da memoria esta
+mov [0h], 012Bh;  IP
   
 int 00h;
       ;Configurando a int 01h
-mov [4], 0700h;
-mov [2], 0138h;
+mov AX, 0000h;   
+mov DS, AX;
+
+mov [6h], 0700h;
+mov [4h], 45h;
+mov [5h], 01h;
 
 int 01h
           
 
 ret
            
-int00:
+int00: 
+    mov DI, 0000h;
+    mov AX, 0FF00h   ;Mudando o registrador DS de lugar para nao
+    mov DS, AX;      ;salvar os caracteres no lugar da int 00 e int 01
+    
     aqui:   
-        mov AX, 0FF00h   ;Mudando o registrador DS de lugar para nao
-        mov DS, AX;      ;salvar os caracteres no lugar da int 00 e int 01
-                 
+                  
         mov AH, 00h;     ;Configurando a int 16h
         int 16h;         ;Int 16 com AH = 00h, espera uma tecla; muda o AH e o AL
         
@@ -34,12 +40,14 @@ int00:
     loop aqui
              
     acabou:
-    mov [di], '$'  
+    mov [di], '$'
+    ret  
 
 int01:
 
-    mov DI, 0000;        
-    
+    mov DI, 0000h;
+    mov AX, 0FF00h   ;Mudando o registrador DS de lugar para nao
+    mov DS, AX;
     _loop:
                     
         mov AH, 02h;
